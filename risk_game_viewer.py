98cea312b40f7c35f909f25114150d231d611b0b
Jacob Mason <jacob@jacobmason.net>
2016-10-18 12:59:43 -0500

    Fixes import error

--- risk_game_viewer.py
+++ risk_game_viewer.py
@@ -20,7 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 from PIL import Image
 from PIL import ImageDraw
-import ImageTk
+from PIL import ImageTk
 import PngImagePlugin #for py2exe
 import Tkinter
 import tkSimpleDialog
